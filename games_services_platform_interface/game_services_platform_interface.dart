c91ae505a9fb05d051c0c7658ee4fe2314c5d10a
Rwema <52289151+rwema3@users.noreply.github.com>
2022-04-05 18:24:16 +0200

    Update game_services_platform_interface.dart

--- games_services_platform_interface/game_services_platform_interface.dart
+++ games_services_platform_interface/game_services_platform_interface.dart
@@ -63,3 +63,4 @@ abstract class GamesServicesPlatform extends PlatformInterface {
 
   /// It will open the leaderboards screen.
   Future<String?> showLeaderboards({iOSLeaderboardID = "", androidLeaderboardID = ""}) async {
+    
