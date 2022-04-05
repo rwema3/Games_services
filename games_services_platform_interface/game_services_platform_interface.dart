63efc1d3cd4f0ba9c0056cb601ce3f64d144c553
Rwema <52289151+rwema3@users.noreply.github.com>
2022-04-05 15:08:14 +0200

    Update game_services_platform_interface.dart

--- games_services_platform_interface/game_services_platform_interface.dart
+++ games_services_platform_interface/game_services_platform_interface.dart
@@ -56,4 +56,4 @@ abstract class GamesServicesPlatform extends PlatformInterface {
     throw UnimplementedError("not implemented.");
   }
 
-  /// It will open the achie
+  /// It will open the achi
