6d14b6aabf8f276e7670186624f88c722059341f
Rwema <52289151+rwema3@users.noreply.github.com>
2022-04-05 13:16:17 +0200

    Update game_services_platform_interface.dart

--- games_services_platform_interface/game_services_platform_interface.dart
+++ games_services_platform_interface/game_services_platform_interface.dart
@@ -85,4 +85,4 @@ abstract class GamesServicesPlatform extends PlatformInterface {
   }
 
   /// Show the iOS Access Point.
-  Future<String?> showAccessPoint(AccessPoin
+  Future<String?> showAccessPoint(AccessPoi
