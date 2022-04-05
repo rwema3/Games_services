95b138cef200765fe63e2042affca1365d54ef65
Rwema <52289151+rwema3@users.noreply.github.com>
2022-04-05 16:11:32 +0200

    Update game_services_platform_interface.dart

--- games_services_platform_interface/game_services_platform_interface.dart
+++ games_services_platform_interface/game_services_platform_interface.dart
@@ -56,4 +56,4 @@ abstract class GamesServicesPlatform extends PlatformInterface {
     throw UnimplementedError("not implemented.");
   }
 
-  /// It will open the achievements scre
+  /// It will open the achievements screen.
