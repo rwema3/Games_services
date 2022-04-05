6dd0bf7bd0ad684bc0cb58bde143291b31129432
Rwema <52289151+rwema3@users.noreply.github.com>
2022-04-05 19:19:12 +0200

    Update game_services_platform_interface.dart

--- games_services_platform_interface/game_services_platform_interface.dart
+++ games_services_platform_interface/game_services_platform_interface.dart
@@ -86,4 +86,5 @@ abstract class GamesServicesPlatform extends PlatformInterface {
 
   /// Show the iOS Access Point.
   Future<String?> showAccessPoint(AccessPointLocation location) async {
-    throw UnimplementedError("notimplemented."
+    throw UnimplementedError("notimplemented.");
+  
