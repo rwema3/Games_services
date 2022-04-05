368b0ca8df195c812ac69416c36a2f7663636b39
Rwema <52289151+rwema3@users.noreply.github.com>
2022-04-05 11:44:34 +0200

    Update game_services_platform_interface.dart

--- games_services_platform_interface/game_services_platform_interface.dart
+++ games_services_platform_interface/game_services_platform_interface.dart
@@ -91,4 +91,4 @@ abstract class GamesServicesPlatform extends PlatformInterface {
 
   /// Hide the iOS Access Point.
   Future<String?> hideAccessPoint() async {
-    throw UnimplementedError("not impl
+    throw UnimplementedError("not imp
