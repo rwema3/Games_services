513df6cb88a1baff987ba9e105ce583954096d83
Rwema <52289151+rwema3@users.noreply.github.com>
2022-04-05 19:18:01 +0200

    Update game_services_platform_interface.dart

--- games_services_platform_interface/game_services_platform_interface.dart
+++ games_services_platform_interface/game_services_platform_interface.dart
@@ -84,4 +84,5 @@ abstract class GamesServicesPlatform extends PlatformInterface {
     throw UnimplementedError("not implemented.");
   }
 
-  /// Show the iOS Access Point
+  /// Show the iOS Access Point.
+  
