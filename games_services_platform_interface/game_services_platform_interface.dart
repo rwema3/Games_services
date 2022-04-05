19bf2bb803e3102a51a5a4476395dcc65477da55
Rwema <52289151+rwema3@users.noreply.github.com>
2022-04-05 17:33:41 +0200

    Update game_services_platform_interface.dart

--- games_services_platform_interface/game_services_platform_interface.dart
+++ games_services_platform_interface/game_services_platform_interface.dart
@@ -61,4 +61,4 @@ abstract class GamesServicesPlatform extends PlatformInterface {
     throw UnimplementedError("not implemented.");
   }
 
-  /// It will open the leaderboards sc
+  /// It will open the leaderboards scr
