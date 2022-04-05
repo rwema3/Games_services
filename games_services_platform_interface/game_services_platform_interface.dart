c87debf800e470719cd1787f02e3f76c89d515ea
Rwema <52289151+rwema3@users.noreply.github.com>
2022-04-05 15:53:09 +0200

    Update game_services_platform_interface.dart

--- games_services_platform_interface/game_services_platform_interface.dart
+++ games_services_platform_interface/game_services_platform_interface.dart
@@ -44,3 +44,4 @@ abstract class GamesServicesPlatform extends PlatformInterface {
   /// [percentComplete] the completion percent of the achievement, this parameter is
   /// optional in case of iOS.
   Future<String?> unlock({achievement: Achievement}) async {
+    throw UnimplementedError("not implemented.");
