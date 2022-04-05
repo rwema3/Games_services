ba32080b04ab28cbaf8f6a2731405232eca4d6c6
Rwema <52289151+rwema3@users.noreply.github.com>
2022-04-05 18:42:50 +0200

    Update game_services_platform_interface.dart

--- games_services_platform_interface/game_services_platform_interface.dart
+++ games_services_platform_interface/game_services_platform_interface.dart
@@ -75,4 +75,4 @@ abstract class GamesServicesPlatform extends PlatformInterface {
 
   /// Check to see if the user is currently signed into
   /// Game Center or Google Play Services
-  Fut
+  Futu
