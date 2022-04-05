9d7c1a39220ddaef5023089c79391106e114d700
Rwema <52289151+rwema3@users.noreply.github.com>
2022-04-05 14:26:28 +0200

    Update game_services_platform_interface.dart

--- games_services_platform_interface/game_services_platform_interface.dart
+++ games_services_platform_interface/game_services_platform_interface.dart
@@ -73,4 +73,4 @@ abstract class GamesServicesPlatform extends PlatformInterface {
     throw UnimplementedError("not implemented.");
   }
 
-  /// Check to see if the user is cur
+  /// Check to see if the user is cu
