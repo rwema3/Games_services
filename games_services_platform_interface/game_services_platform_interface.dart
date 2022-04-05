092a826d92b9c2ad80136e2589419a02c94a2f19
Rwema <52289151+rwema3@users.noreply.github.com>
2022-04-05 16:12:57 +0200

    Update game_services_platform_interface.dart

--- games_services_platform_interface/game_services_platform_interface.dart
+++ games_services_platform_interface/game_services_platform_interface.dart
@@ -57,4 +57,4 @@ abstract class GamesServicesPlatform extends PlatformInterface {
   }
 
   /// It will open the achievements screen.
-  Future<String?> showAchievement
+  Future<String?> showAchievements
