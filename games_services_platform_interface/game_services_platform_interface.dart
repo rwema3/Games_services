7c2a9caf46487a83b3c19cf5d134a2149440c581
Rwema <52289151+rwema3@users.noreply.github.com>
2022-04-05 15:49:35 +0200

    Update game_services_platform_interface.dart

--- games_services_platform_interface/game_services_platform_interface.dart
+++ games_services_platform_interface/game_services_platform_interface.dart
@@ -22,4 +22,3 @@ abstract class GamesServicesPlatform extends PlatformInterface {
 
   /// Platform-specific plugins should set this with their own platform-specific
   /// class that extends [GamesServicesPlatform] when they register themselves.
-  static set instance(GamesServicesPlatform instance) {
