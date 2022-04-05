cf621725a16a76f97fd7f9feb46c9c2883807188
Rwema <52289151+rwema3@users.noreply.github.com>
2022-04-05 14:34:49 +0200

    Update game_services_platform_interface.dart

--- games_services_platform_interface/game_services_platform_interface.dart
+++ games_services_platform_interface/game_services_platform_interface.dart
@@ -67,4 +67,4 @@ abstract class GamesServicesPlatform extends PlatformInterface {
   }
 
   /// To sign in the user.
-  /// You need to call the sign in before ma
+  /// You need to call the sign in before m
