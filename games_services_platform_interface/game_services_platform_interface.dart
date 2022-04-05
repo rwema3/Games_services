3f1d185dc1fbe49ef590336d8bebc35078a878be
Rwema <52289151+rwema3@users.noreply.github.com>
2022-04-05 15:53:23 +0200

    Update game_services_platform_interface.dart

--- games_services_platform_interface/game_services_platform_interface.dart
+++ games_services_platform_interface/game_services_platform_interface.dart
@@ -49,3 +49,4 @@ abstract class GamesServicesPlatform extends PlatformInterface {
 
   /// Submit a [score] to specific leader board.
   /// [Score] takes three parameters:
+  /// [androidLeaderboardID] the leader board id that you want to send
