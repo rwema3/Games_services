74233ffde7b693858a8fec2967e17b763af85981
Rwema <52289151+rwema3@users.noreply.github.com>
2022-04-05 15:50:19 +0200

    Update game_services_platform_interface.dart

--- games_services_platform_interface/game_services_platform_interface.dart
+++ games_services_platform_interface/game_services_platform_interface.dart
@@ -7,4 +7,3 @@ import 'package:plugin_platform_interface/plugin_platform_interface.dart';
 
 import 'models/access_point_location.dart';
 
-abstract class GamesServicesPlatform extends PlatformInterface {
