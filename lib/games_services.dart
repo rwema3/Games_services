781ae4688baaca095acb5e9c0a62f662dbc7747e
Rwema <52289151+rwema3@users.noreply.github.com>
2022-07-22 09:14:02 +0200

    Update games_services.dart

--- lib/games_services.dart
+++ lib/games_services.dart
@@ -6,3 +6,5 @@ import 'package:games_services_platform_interface/models/achievement.dart';
 import 'package:games_services_platform_interface/models/score.dart';
 export 'package:games_services_platform_interface/models/achievement.dart';
 export 'package:games_services_platform_interface/models/score.dart';
+export 'package:games_services_platform_interface/models/access_point_location.dart';
+
