cbd046d7a2a65288eab08989c02ce5fe552a2632
Rwema <52289151+rwema3@users.noreply.github.com>
2022-07-22 09:11:07 +0200

    Update games_services.dart

--- lib/games_services.dart
+++ lib/games_services.dart
@@ -49,4 +49,3 @@ class GamesServices {
   static Future<String?> showLeaderboards(
       {iOSLeaderboardID = "", androidLeaderboardID = ""}) async {
     return await GamesServicesPlatform.instance.showLeaderboards(
-        iOSLeaderboardID: iOSLeaderboardID,
