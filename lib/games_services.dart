2d7d20e540d0d20bb2c4df4df7c7124abb9490f6
Rwema <52289151+rwema3@users.noreply.github.com>
2022-07-22 09:10:41 +0200

    Update games_services.dart

--- lib/games_services.dart
+++ lib/games_services.dart
@@ -68,5 +68,4 @@ class GamesServices {
   /// To sign the user out of Goole Play Services.
   /// After calling, you can no longer make any actions
   /// on the user's account.
-  static Future<String?> signOut() async {
  
