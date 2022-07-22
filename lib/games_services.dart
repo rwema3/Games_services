b58b6b015ddc581eeb1f86b885523c9c06668650
Rwema <52289151+rwema3@users.noreply.github.com>
2022-07-22 09:15:39 +0200

    Update games_services.dart

--- lib/games_services.dart
+++ lib/games_services.dart
@@ -63,4 +63,6 @@ class GamesServices {
   /// Check to see if the user is currently signed into
   /// Game Center or Google Play Services
   static Future<bool> get isSignedIn async =>
+      await GamesServicesPlatform.instance.isSignedIn ?? false;
+
  
