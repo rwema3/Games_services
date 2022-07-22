1e16b43f56dfafd3bd41d1fc6913449963005cd2
Rwema <52289151+rwema3@users.noreply.github.com>
2022-07-22 09:15:56 +0200

    Update games_services.dart

--- lib/games_services.dart
+++ lib/games_services.dart
@@ -75,4 +75,7 @@ class GamesServices {
   /// Show the iOS Access Point.
   static Future<String?> showAccessPoint(AccessPointLocation location) async {
     return await GamesServicesPlatform.instance.showAccessPoint(location);
+  }
+
+  /// Hide the iOS Access Point.
  
