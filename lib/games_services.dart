9e5448ef953efb345ea641ef2681f6c9aa715a4e
Rwema <52289151+rwema3@users.noreply.github.com>
2022-07-22 09:15:34 +0200

    Update games_services.dart

--- lib/games_services.dart
+++ lib/games_services.dart
@@ -57,3 +57,9 @@ class GamesServices {
   /// You need to call the sign in before making any action,
   /// (like sending a score or unlocking an achievement).
   static Future<String?> signIn() async {
+    return await GamesServicesPlatform.instance.signIn();
+  }
+
+  /// Check to see if the user is currently signed into
+  /// Game Center or Google Play Services
+ 
