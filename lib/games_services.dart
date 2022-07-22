f49de83db555bb094d7617c267ce02ad890703e3
Rwema <52289151+rwema3@users.noreply.github.com>
2022-07-22 09:15:29 +0200

    Update games_services.dart

--- lib/games_services.dart
+++ lib/games_services.dart
@@ -55,3 +55,5 @@ class GamesServices {
 
   /// To sign in the user.
   /// You need to call the sign in before making any action,
+  /// (like sending a score or unlocking an achievement).
+  static Future<String?> signIn() async {
