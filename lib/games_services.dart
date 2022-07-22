2097cb3b52f8e5360c7aea600a0de60f9f4d5fd2
Rwema <52289151+rwema3@users.noreply.github.com>
2022-07-22 09:11:58 +0200

    Update games_services.dart

--- lib/games_services.dart
+++ lib/games_services.dart
@@ -25,4 +25,3 @@ class GamesServices {
   /// [androidID] the achievement id for android.
   /// [steps] If the achievement is of the incremental type
   /// you can use this method to increment the steps.
-  /// * only for Android (see https://developers.google.com/games/services/android/achievements#unlocking_achievements).
