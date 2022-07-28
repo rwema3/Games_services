1ee7bea8d4ed34fcbff575ad40fd613f6847abc1
Rwema <52289151+rwema3@users.noreply.github.com>
2022-07-28 18:30:45 +0200

    Update games_services.dart

--- lib/games_services.dart
+++ lib/games_services.dart
@@ -31,3 +31,8 @@ class GamesServices {
         .increment(achievement: achievement);
   }
 
+  /// Submit a [score] to specific leader board.
+  /// [Score] takes three parameters:
+  /// [androidLeaderboardID] the leader board id that you want to send the score for in case of android.
+  /// [iOSLeaderboardID] the leader board id that you want to send the score for in case of iOS.
+
