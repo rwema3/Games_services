487c19006d81801bea9a9cb9dbfc14c60fc50f74
Rwema <52289151+rwema3@users.noreply.github.com>
2022-07-28 18:31:36 +0200

    Update games_services.dart

--- lib/games_services.dart
+++ lib/games_services.dart
@@ -60,6 +60,10 @@ class GamesServices {
     return await GamesServicesPlatform.instance.signIn();
   }
 
+  /// Check to see if the user is currently signed into
+  /// Game Center or Google Play Services
+  static Future<bool> get isSignedIn async =>
+      await GamesServicesPlatform.instance.isSignedIn ?? false;
 
   /// To sign the user out of Goole Play Services.
   /// After calling, you can no longer make any actions
