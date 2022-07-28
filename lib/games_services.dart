0bdedbfbac444bf846279cb9e60f9a8b579b0ea7
Rwema <52289151+rwema3@users.noreply.github.com>
2022-07-28 18:29:39 +0200

    Update games_services.dart

--- lib/games_services.dart
+++ lib/games_services.dart
@@ -45,8 +45,3 @@ class GamesServices {
     return await GamesServicesPlatform.instance.showAchievements();
   }
 
-  /// It will open the leaderboards screen.
-  static Future<String?> showLeaderboards(
-      {iOSLeaderboardID = "", androidLeaderboardID = ""}) async {
-    return await GamesServicesPlatform.instance.showLeaderboards(
-  
