e95e79fe7f9d26bd86b8711ea65b2a13b1c40d91
Dominique Rwema Bagirishya <52289151+rwema3@users.noreply.github.com>
2022-08-27 21:49:46 -0700

    class GS

--- lib/games_services.dart
+++ lib/games_services.dart
@@ -8,6 +8,17 @@ export 'package:games_services_platform_interface/models/achievement.dart';
 export 'package:games_services_platform_interface/models/score.dart';
 export 'package:games_services_platform_interface/models/access_point_location.dart';
 
+class GamesServices {
+  /// Unlock an [achievement].
+  /// [Achievement] takes three parameters:
+  /// [androidID] the achievement id for android.
+  /// [iOSID] the achievement id for iOS.
+  /// [percentComplete] the completion percent of the achievement, this parameter is
+  /// optional in case of iOS.
+  static Future<String?> unlock({achievement: Achievement}) async {
+    return await GamesServicesPlatform.instance
+        .unlock(achievement: achievement);
+  }
 
   /// Increment an [achievement].
   /// [Achievement] takes two parameters:
